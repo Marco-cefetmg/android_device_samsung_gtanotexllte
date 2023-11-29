57a5218b587176f895207fc725df6e3ed92bb761
Marco-cefetmg <marcoaureli.o@outlook.com>
2023-11-29 19:47:06 -0300

    fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,88 +1,92 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-DEVICE_PATH := device/samsung/gtanotexllte
+LOCAL_PATH := device/samsung/gtanotexlwifi
 
-# For building with minimal manifest
-ALLOW_MISSING_DEPENDENCIES := true
+# Platform
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_SOC := exynos7870
+TARGET_BOARD_PLATFORM_GPU := mali-t830mp2
+TARGET_BOOTLOADER_BOARD_NAME := universal7870
+TARGET_NO_BOOTLOADER := true
 
-# Architecture
+# CPU
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := generic
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_CORTEX_A53 := true
 
-TARGET_USES_64_BIT_BINDER := true
-
-# APEX
-#OVERRIDE_TARGET_FLATTEN_APEX := true
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := universal7870
-TARGET_NO_BOOTLOADER := true
-
-# Display
-TARGET_SCREEN_DENSITY := 240
 
 # Kernel
-BOARD_KERNEL_BASE := 0x10000000
-# BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE :=
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --board SRPPG02A000RU
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CONFIG := exynos7870-gtanotexlwifi_defconfig
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/samsung/P580KXU1CRJ7
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+KERNEL_TOOLCHAIN := /home/unknowwiiplayer/Desktop/Kernels/93092903254cba0b6950c94170332973a85b450f/bin
+
+# Image
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/gtanotexlwifi/mkbootimg.mk
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
-TARGET_KERNEL_CONFIG := gtanotexllte_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/gtanotexllte
-
-# Kernel - prebuilt
-TARGET_FORCE_PREBUILT_KERNEL := true
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-TARGET_PREBUILT_DT := $(DEVICE_PATH)/prebuilt/dt.img
-BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DT)
-BOARD_KERNEL_SEPARATED_DT := 
-endif
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPPG02A000RU
+TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BOOTIMAGE_PARTITION_SIZE := 23451936
-# BOARD_RECOVERYIMAGE_PARTITION_SIZE := 23451936
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x002000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x002600000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x0DF800000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x2A5FFB000 # 0x2A6000000 - 20480 (footer)
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x00C800000
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE  := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-
-# Platform
-TARGET_BOARD_PLATFORM := exynos5
-
-# Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# Security patch level
-VENDOR_SECURITY_PATCH := 2021-08-01
-
-# Hack: prevent anti rollback
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
-
-# TWRP Configuration
+# TWRP specific build flags
+TARGET_RECOVERY_FSTAB := device/samsung/gtanotexlwifi/recovery.fstab
 TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
+RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/13600000.usb/13600000.dwc3/gadget/lun%d/file"
+TW_BRIGHTNESS_PATH := "/sys/devices/14800000.dsim/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 162
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_NTFS_3G := true
+#TW_NO_EXFAT_FUSE := true
+TW_EXCLUDE_SUPERSU := true
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+# Samsung's encryption is currently unsupported
+#TW_INCLUDE_CRYPTO_SAMSUNG := true
+#TARGET_HW_DISK_ENCRYPTION := true
+
+TARGET_SYSTEM_PROP := device/samsung/gtanotexlwifi/system.prop
+
+# Include
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/gtanotexlwifi/include
+
+
--- README.md
+++ README.md
@@ -1,10 +1,30 @@
-# Android device tree for samsung SM-P585M (gtanotexllte)
+# TWRP device tree for Samsung Tab A 10.1 With S-Pen (2016) (gtanotexlwifi)
 
+## Kernel source available at 
+https://github.com/unknowwiiplayer/kernel_samsung_exynos7870
+
+# How to build
+## Setup build environment
+
+Create a workspace and open a terminal
+
+    repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-8.1
+    
+Add to `.repo/local_manifests/roomservice.xml`:
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+	<project name="unknowwiiplayer/android_device_samsung_gtanotexlwifi" path="device/samsung/gtanotexlwifi" remote="github" revision="android-8.1"/>
+	<project name="unknowwiiplayer/android_kernel_samsung_exynos7870" path="kernel/samsung/exynos7870" remote="github" revision="android-8.1" />
+</manifest>
 ```
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-```
+Sync
+
+    repo sync
+    
+Define your TARGET_KERNEL_CROSS_COMPILE_PREFIX and KERNEL_TOOLCHAIN in device/samsung/gtanotexlwifi/BoardConfig.mk
+   
+## Build time
+    
+    cd <source-dir>; export ALLOW_MISSING_DEPENDENCIES=true; export ANDROID_MAJOR_VERSION=o; . build/envsetup.sh; lunch omni_<device>-eng; mka recoveryimage
